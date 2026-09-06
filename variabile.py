17467bd61c9a1a4bfad529ac9f403551de156c4f
Ion13135 <sobraneschiion3@gmail.com>
2026-09-06 02:30:17 +0200

    Exercitiu variabile si tipuri de date

---
+++ variabile.py
@@ -0,0 +1,12 @@
+# Creez variabilele cerute
+nume = "Ion"
+varsta = 20
+inaltime = 1.78
+
+# Afisez propozitia cu f-string
+print(f"Numele meu este {nume},  am {varsta} ani si sunt inalt de {inaltime} metri.")
+
+# Calculez anul in care voi implini 100 de ani
+an_curent = 2026
+an_100 = an_curent - varsta + 100
+print(f"Voi implini 100 de ani in anul {an_100}")
