780a820b9d7a0809a61460411d306344b84e07ef
Ion13135 <sobraneschiion3@gmail.com>
2026-09-09 17:21:50 +0200

    Exercitiu if-else verificare varsta cu teste multiple

---
+++ verificare_varsta.py
@@ -0,0 +1,13 @@
+varsta = int(input("Ce varsta ai? "))
+
+# verificare drept de vot
+if varsta >= 18:
+    print("Poti vota ")
+else:
+    print("Nu poti vota ")
+
+# verificare permis categoria B
+if varsta >= 18:
+    print("Poti obtine permis categoria B ")
+else:
+    print("Nu poti obtine permis categoria B ")
